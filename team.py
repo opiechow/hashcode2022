cd2dd089d1e821d0754348e430960157bd4c0a32
Michal <abc@abc.abc>
2022-02-24 21:30:25 +0100

    it doesnt explode!

--- team.py
+++ team.py
@@ -4,7 +4,7 @@ class Contributor:
     def __init__(self):
         self.name = ""
         self.no_skills = 0
-        self.skills = {}
+        self.skills = []
         self.first_day_available = 0
 
 class Project:
@@ -15,16 +15,18 @@ class Project:
         self.deadline = deadline
         self.no_roles = no_roles
         self.roles = []
+        self.mentored_roles = []
+        self.assigned_roles = []
         self.assigned = False
 
 class ProjectRole:
-    def __init__(self, name, required_level, index):
-        self.name = name
-        self.required_level = required_level
+    def __init__(self, skill, index):
+        self.skill = skill
         self.index = index
 
 contributors = []
 projects = []
+skills = {}
 
 in_file = "team_inputs/a_an_example.in.txt"
 
@@ -37,8 +39,13 @@ with open(in_file, "r") as f:
         contributor.no_skills = int(no_skills)
         contributor.name = name
         for j in range(contributor.no_skills):
-            skill_name, skill_level = f.readline().split()
-            contributor.skills[skill_name] = skill_level
+            # skill is name + level
+            skill = f.readline().rstrip()
+            contributor.skills.append(skill)
+            if skill in skills:
+                skills[skill].append(contributor)
+            else:
+                skills[skill] = [contributor]
         contributors.append(contributor)
 
 
@@ -46,8 +53,8 @@ with open(in_file, "r") as f:
         name, no_days, score, deadline, no_roles = f.readline().split()
         project = Project(name, int(no_days), int(score), int(deadline), int(no_roles))
         for j in range(project.no_roles):
-            name, required_lvl = f.readline().split()
-            project.roles.append(ProjectRole(name,int(required_lvl),int(j)))
+            skill = f.readline().rstrip()
+            project.roles.append(ProjectRole(skill, int(j)))
 
         projects.append(project)
 
@@ -57,8 +64,34 @@ for day in range(max_day):
     for project in projects:
         if project.assigned:
             continue
-        possible_assignments = {}
-        for contributor in filter(lambda x: x.first_day_available <= day, contributors):
+        for role in project.roles:
+            if role.skill in skills:
+                avail_conts = list(filter(lambda x: x.first_day_available <= day and x not in map(lambda y: y[0],project.assigned_roles) , skills[role.skill]))
+                if not avail_conts:
+                    #TODO: mentor
+                    break
+                avail_conts.sort(key = lambda x: x.no_skills)
+                the_chosen_one = avail_conts[-1]
+                project.assigned_roles.append((the_chosen_one, role.index, role.skill))
+        if len(project.assigned_roles) == len(project.roles):
+            for asignee, idx, oldskill in project.assigned_roles:
+                asignee.first_day_available = day + project.no_days
+                #todo: higher levels
+                skill_name,level = oldskill.split()
+                newskill = skill_name + " " + str(int(level)+1)
+                asignee.skills = list(filter(lambda x: x != oldskill, asignee.skills))
+                asignee.skills.append(newskill)
+
+                skills[oldskill].remove(asignee)
+                if newskill in skills:
+                    skills[newskill].append(asignee)
+                else:
+                    skills[newskill] = [asignee]
+
+            project.assigned = True
+
+
+        """ for contributor in filter(lambda x: x.first_day_available <= day, contributors):
             role_set = project.roles.map(lambda x: x.name)
             skill_set = set(contributor.skills.keys())
             common_skills = skill_set.intersection(role_set)
@@ -69,7 +102,7 @@ for day in range(max_day):
                     #contributor.first_day_available = day + project.no_days
                     possible_assignments[common_skill] = contributor.name
                     continue
-        
+ """
 
 
 pass
